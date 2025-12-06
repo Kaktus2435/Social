33194b3e6bcba0c9cfbbea36dc6258535b906d78
Paereli Andrian <andrian.paereli@mail.ru>
2025-12-06 13:50:01 +0300

    FixLoadPath

--- src/i18n.js
+++ src/i18n.js
@@ -3,41 +3,18 @@ import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 
-
-/* import en from './locales/en/main.json';
-import ro from './locales/ro/main.json';
- */
 i18n
   .use(Backend)
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-/*     supportedLngs: ['en', 'ro'],
-    fallbackLng: 'en',
-    ns: ['common', 'profile', 'main'],
-    defaultNS: 'common',
-    debug: true,
-      interpolation: {
-      escapeValue: false,
-    },
 
-    resources: {
-      ro: {
-        main: ro,
-        profile: ro
-      },
-      en: {
-        main: en,
-        profile: en
-      },
-    },
- */
 
     lng: 'en',
     ns: ['main', 'profile'],
 
     backend: {
-      loadPath: '/locales/{{lng}}/{{ns}}.json',
+      loadPath: 'https://Kaktus2435.github.io/Social' + '/locales/{{lng}}/{{ns}}.json',
     },
   });
 
